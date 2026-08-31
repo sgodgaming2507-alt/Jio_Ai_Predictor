284229116d3f67458bc962a0cd32fbe07b465bf3
sgodgaming2507-alt <sgodgaming2507@gmail.com>
2026-08-31 15:20:51 +0530

    Update MainActivity.kt

--- app/src/main/java/org/sgod/jioai/MainActivity.kt
+++ app/src/main/java/org/sgod/jioai/MainActivity.kt
@@ -1,1 +1,31 @@
 
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.provider.Settings
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+        // Simple UI layout programmatically create kar rahe hain taaki alag se XML na banani pade
+        val btn = Button(this).apply {
+            text = "Start Floating Predictor"
+            setOnClickListener {
+                if (Settings.canDrawOverlays(this@MainActivity)) {
+                    startService(Intent(this@MainActivity, FloatingService::class.java))
+                    Toast.makeText(this@MainActivity, "Service Started!", Toast.LENGTH_SHORT).show()
+                    finish()
+                } else {
+                    val intent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                        Uri.parse("package:$packageName"))
+                    startActivity(intent)
+                }
+            }
+        }
+        setContentView(btn)
+    }
+}
